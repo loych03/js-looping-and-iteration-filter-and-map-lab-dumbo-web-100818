92b58cb75b8ca539f2da1b736db5f3c9182a5f7b
github username <github email address>
2018-11-20 21:36:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, oldRevenue){
+  return drivers.filter(function(driver){return driver.revenue> oldRevenue });
+
+}
+
+function driverNamesWithRevenueOver(drivers, oldRevenue){
+  return driversWithRevenueOver(drivers, oldRevenue).map(driver =>driver.name);
+}
+
+function exactMatch(drivers, key){
+  return drivers.filter(driver=>
+  driver.name === key.name || driver.revenue === key.revenue);
+}
+
+function exactMatchToList(drivers, key){
+  return exactMatch(drivers, key).map(driver => driver.name );
+
+}
